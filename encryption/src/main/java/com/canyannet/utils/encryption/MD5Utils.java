a11ae770f793b643552288b139cee9322ce1d368
leo <firelion0725@163.com>
2018-07-25 16:23:37 +0800

    增加MD5加密值

--- encryption/src/main/java/com/canyannet/utils/encryption/MD5Utils.java
+++ encryption/src/main/java/com/canyannet/utils/encryption/MD5Utils.java
@@ -1,5 +1,11 @@
 package com.canyannet.utils.encryption;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.security.MessageDigest;
 
 /**
@@ -34,5 +40,36 @@ public class MD5Utils {
             hexValue.append(Integer.toHexString(val));
         }
         return hexValue.toString();
+    }
+
+    /**
+     * 文件MD5加密
+     *
+     * @param file 文件
+     * @return MD加密值
+     */
+    public static String getMd5ByFile(File file) {
+        String value = "";
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream(file);
+            MappedByteBuffer byteBuffer;
+            byteBuffer = in.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, file.length());
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            md5.update(byteBuffer);
+            BigInteger bi = new BigInteger(1, md5.digest());
+            value = bi.toString(16);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != in) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return value;
     }
 } 
